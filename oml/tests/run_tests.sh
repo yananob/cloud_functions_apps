804e44c7f02930290e8eab84cd88ba4101066fde
yananob <yananob@gmail.com>
2024-03-20 23:19:28 +0900

    cf fix tests #23

--- oml/tests/run_tests.sh
+++ oml/tests/run_tests.sh
@@ -3,7 +3,7 @@ set -e
 
 ./vendor/bin/phpstan analyze -c phpstan.neon
 
-./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --testdox tests/
+./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --display-errors tests/
 # ./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --testdox tests/CrawlerTest.php
 # ./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --testdox tests/AccountsTest.php
 # ./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --testdox tests/OmlBookTest.php
