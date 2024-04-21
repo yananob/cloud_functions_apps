a9baac2a4640ef9cc5cc3f56942c4d2fe57bf429
yananob <yananob@gmail.com>
2024-04-21 15:41:36 +0900

    [oml] 予約のあと、リストに即時反映する #4

--- oml/src/CacheItems.php
+++ oml/src/CacheItems.php
@@ -7,6 +7,5 @@ enum CacheItems: string
     case Accounts = "oml_accounts";
     case ReservedBooks = "oml_reservedbooks";
     case LendingBooks = "oml_lendingbooks";
-    case ReservedCount = "oml_reservedcount";
     case UpdatedTimestamps = "oml_updatedtimestamps";
 }
