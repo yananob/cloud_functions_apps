b790f396e835f2420896510f60f4dbcda4f9d2df
yananob <yananob@gmail.com>
2024-11-15 21:28:32 +0900

    update

--- webhook-receive/tests/run_tests.sh
+++ webhook-receive/tests/run_tests.sh
@@ -4,10 +4,10 @@ set -eu
 echo "Running PHPStan..."
 ./vendor/bin/phpstan analyze -c phpstan.neon
 
-TEST_TARGET=""
-if [ $# -eq 1 ];then
-    TEST_TARGET=$1.php
-fi
+# TEST_TARGET=""
+# if [ $# -eq 1 ];then
+#     TEST_TARGET=$1.php
+# fi
 
-echo "Running PHPUnit $TEST_TARGET..."
-./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --display-errors tests/$TEST_TARGET
+# echo "Running PHPUnit $TEST_TARGET..."
+# ./vendor/bin/phpunit --colors=auto --display-notices --display-warnings --display-errors tests/$TEST_TARGET
