0c5848086a12a2047865b94c17fbf1596178a440
yananob <yananob@gmail.com>
2024-11-16 13:23:31 +0900

    Switch to LINE messaging API #94

--- iijmio-usage/index.php
+++ iijmio-usage/index.php
@@ -2,23 +2,31 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-Google\CloudFunctions\FunctionsFramework::cloudEvent('main', 'main');
-function main(\CloudEvents\V1\CloudEventInterface $event): void
-{
-    $logger = new yananob\mytools\Logger();
+use Google\CloudFunctions\FunctionsFramework;
+use CloudEvents\V1\CloudEventInterface;
+use yananob\mytools\Logger;
+use yananob\my_gcptools\CFUtils;
+use yananob\mytools\Utils;
+use yananob\mytools\Line;
+use MyApp\IijmioUsage;
 
-    $isLocal = yananob\my_gcptools\GcpUtils::isLocalEvent($event);
+FunctionsFramework::cloudEvent('main', 'main');
+function main(CloudEventInterface $event): void
+{
+    $logger = new Logger();
+
+    $isLocal = CFUtils::isLocalEvent($event);
     $logger->log("Running as " . ($isLocal ? "local" : "cloud") . " mode");
 
-    $config = yananob\mytools\Utils::getConfig(__DIR__ . "/configs/config.json");
-    $iijmio = new MyApp\IijmioUsage(
+    $config = Utils::getConfig(__DIR__ . "/configs/config.json");
+    $iijmio = new IijmioUsage(
         $config["iijmio"], $config["alert"]["send_usage_each_n_days"]
     );
     $result = $iijmio->callApi();
     $alert_info = $iijmio->judgeResult($result);
     if ($alert_info["isSend"]) {
-        $line = new \yananob\mytools\Line(__DIR__ . '/configs/line.json');
-        $line->sendMessage($config["alert"]["target"], $alert_info["message"]);
+        $line = new Line(__DIR__ . '/configs/line.json');
+        $line->sendMessage($config["alert"]["bot"], $config["alert"]["target"], $alert_info["message"]);
     }
 
     $logger->log($alert_info["message"]);
