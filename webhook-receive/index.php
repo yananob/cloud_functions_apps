4688d3051a6795d0437530fd77708e4f505b922d
yananob <yananob@gmail.com>
2024-11-23 19:30:23 +0900

    Webhook-respond-line (#102)
    * added responsing message
    
    * added responsing message
    
    * added responsing message

--- webhook-receive/index.php
+++ webhook-receive/index.php
@@ -4,54 +4,48 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Google\CloudFunctions\FunctionsFramework;
 use GuzzleHttp\Psr7\Response;
 use yananob\mytools\Logger;
+use yananob\mytools\Line;
 
-Google\CloudFunctions\FunctionsFramework::http('main', 'main');
+FunctionsFramework::http('main', 'main');
 function main(ServerRequestInterface $request): ResponseInterface
 {
     $logger = new Logger("webhook-receive");
     $logger->log(str_repeat("-", 120));
-
     $logger->log("headers: " . json_encode($request->getHeaders()));
-
     $logger->log("params: " . json_encode($request->getQueryParams()));
-
     $logger->log("parsedBody: " . json_encode($request->getParsedBody()));
-
     $body = $request->getBody()->getContents();
     $logger->log("body: " . $body);
-    $logger->log("body_json: " . json_encode(json_decode($body)));
+    $body = json_decode($body, false);
 
-    // sample:
-    /* body: 
-    {
-        "destination": "XXXX",
-        "events" : [
-            {
-                "type": "message",
-                "message": {
-                    "type":"text",
-                    "id":"XXXX",
-                    "quoteToken":"XXXX",
-                    "text":"わわわわわ"
-                },
-                "webhookEventId": "XXXX",
-                "deliveryContext": {
-                    "isRedelivery": false
-                },
-                "timestamp":1731926933496,
-                "source":{
-                    "type":"user",
-                    "userId":"XXXX"
-                },
-                "replyToken":"XXXX",
-                "mode":"active"
-            }
-        ]
+    $event = $body->events[0];
+    $message = $event->message->text;
+
+    $type = $event->source->type;
+    $targetId = null;
+    // typeを判定して、idを取得
+    if ($type === 'user') {
+        $targetId = $event->source->userId;
+    } else if ($type === 'group') {
+        $targetId = $event->source->groupId;
+    } else if ($type === 'room') {
+        $targetId = $event->source->roomId;
+    } else {
+        throw new Exception("Unknown type :" + $type);
     }
-    */
 
+    $line = new Line(__DIR__ . "/configs/line.json");
+    $line->sendMessage(
+        bot: "aisan",
+        // target: "dailylog",
+        targetId: $targetId,
+        message: "Got Message: \n" . $message,
+        replyToken: $event->replyToken
+    );
+  
     $headers = ['Content-Type' => 'application/json'];
     return new Response(200, $headers, json_encode($body));
 }
