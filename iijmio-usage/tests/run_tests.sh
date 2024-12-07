cf5c9f6af17e70f2b4099e2f8b3bebcc4bfe6b08
yananob <yananob@gmail.com>
2024-12-07 14:51:20 +0900

    fix errors

--- iijmio-usage/tests/run_tests.sh
+++ iijmio-usage/tests/run_tests.sh
@@ -2,7 +2,7 @@
 set -eu
 
 echo "Running PHPStan..."
-./vendor/bin/phpstan analyze -c ./phpstan.neon .
+./vendor/bin/phpstan analyze -c ./phpstan.neon
 
 ./vendor/bin/phpunit --colors=auto --display-notices --display-warnings tests/
 # ./vendor/bin/phpunit tests/MyHelloTest.php
